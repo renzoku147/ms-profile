001ab5087d9ae34688576179dc00c0f62558131b
renzoku147 <renzoku147@gmail.com>
2021-08-15 07:51:04 -0500

    Api gateway, registry server

--- src/main/java/com/spring/msprofile/entity/TypeCustomer.java
+++ src/main/java/com/spring/msprofile/entity/TypeCustomer.java
@@ -8,7 +8,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
